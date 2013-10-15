99b4c8c41f8049d2dd416af77aa813bff4c2cf5a
gsourdrille@gmail.com <gsourdrille@gmail.com@12e82df5-f569-c33a-2569-eab8e1019727>
2013-10-15 12:00:18 +0000

    [US] Ajout de l'activity settings

--- trunk/TourDeGarde/src/fr/edenyorke/tourdegarde/SettingsActivity.java
+++ trunk/TourDeGarde/src/fr/edenyorke/tourdegarde/SettingsActivity.java
@@ -9,8 +9,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SettingsActivity extends Activity implements OnClickListener{
+	
+	private LinearLayout dateDebutBouton;
+	private LinearLayout dateFinBouton;
+	private LinearLayout periodeBouton;
+	private LinearLayout estPeriodeBouton;
+	private TextView dateDebutValue;
+	private TextView dateFinValue;
+	private TextView periodeValue;
+	private TextView estPeriodeValue;
 	
 	
 	 /** Called when the activity is first created. */
@@ -52,19 +63,33 @@ public class SettingsActivity extends Activity implements OnClickListener{
      * Init ihm component
      */
     private void initScreenComponent() {
-    	
+    	dateDebutBouton = (LinearLayout)findViewById(R.id.dateDebutBouton);
+    	dateFinBouton = (LinearLayout)findViewById(R.id.dateFinBouton);
+    	periodeBouton = (LinearLayout)findViewById(R.id.periodeBouton);
+    	estPeriodeBouton = (LinearLayout)findViewById(R.id.estPeriodeBouton);
     }
     
     /**
      * Init listener
      */
     private void initListener() {
-    	
+    	dateDebutBouton.setOnClickListener(this);
+    	dateFinBouton.setOnClickListener(this);
+    	periodeBouton.setOnClickListener(this);
+    	estPeriodeBouton.setOnClickListener(this);
     }
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		if(v == dateDebutBouton) {
+			
+		}else if(v == dateFinBouton){
+			
+		}else if(v == periodeBouton){
+			
+		}else if(v == estPeriodeBouton){
+			
+		}
 		
 	}
 
