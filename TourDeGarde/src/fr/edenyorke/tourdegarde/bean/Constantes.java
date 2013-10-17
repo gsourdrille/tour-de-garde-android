e27c7d01e9350bdb3d678487741792b5afb3ba9f
gsourdrille@gmail.com <gsourdrille@gmail.com@12e82df5-f569-c33a-2569-eab8e1019727>
2013-10-17 12:49:30 +0000

    [US] modification datepicker

---
+++ TourDeGarde/src/fr/edenyorke/tourdegarde/bean/Constantes.java
@@ -0,0 +1,10 @@
+package fr.edenyorke.tourdegarde.bean;
+
+import android.os.Environment;
+
+public class Constantes {
+	
+	public final static String PATH_DATA = Environment.getExternalStorageDirectory().getAbsolutePath() 
+			+ "/tourDeGarde/gardes.data";;
+
+}
