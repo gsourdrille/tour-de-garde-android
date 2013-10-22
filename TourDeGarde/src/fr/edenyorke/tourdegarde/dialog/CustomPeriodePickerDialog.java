5e3eb1b97948fd19b79eaed9d5c8182bd78d846c
gsourdrille@gmail.com <gsourdrille@gmail.com@12e82df5-f569-c33a-2569-eab8e1019727>
2013-10-22 12:03:50 +0000

    [DEV] creation periodePicker

---
+++ TourDeGarde/src/fr/edenyorke/tourdegarde/dialog/CustomPeriodePickerDialog.java
@@ -0,0 +1,138 @@
+package fr.edenyorke.tourdegarde.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
+import fr.edenyorke.tourdegarde.R;
+import fr.edenyorke.tourdegarde.bean.Periode;
+import fr.edenyorke.tourdegarde.bean.TypePeriode;
+
+public abstract class CustomPeriodePickerDialog extends Dialog implements android.view.View.OnClickListener, OnValueChangeListener{
+	
+	private NumberPicker typePeriodePicker;
+	private NumberPicker numberPeriodePicker;
+	private Button cancelButton;
+	private Button saveButton;
+	private Periode periode;
+	private String[] typePeriodeArray = new String[] { "1 semaine sur", "1 mois sur", "1 fois sur" };
+
+	public CustomPeriodePickerDialog(Context context, Periode currentPeriode) {
+		super(context);
+		setContentView(R.layout.custom_periodepicker_dialog);
+		setCancelable(true);
+		
+		periode = currentPeriode;
+		
+		typePeriodePicker = (NumberPicker) findViewById(R.id.customTypePeriode);
+		typePeriodePicker.setMinValue(0);
+		typePeriodePicker.setMaxValue(2);
+		typePeriodePicker.setDisplayedValues( typePeriodeArray);
+		typePeriodePicker.setOnValueChangedListener(this);
+		switch (currentPeriode.getTypePeriode()) {
+		case WEEK:
+			typePeriodePicker.setValue(0);
+			break;
+		case MONTH:
+			typePeriodePicker.setValue(1);
+		case TIMES:
+			typePeriodePicker.setValue(2);
+			break;
+		default:
+			break;
+		}
+		
+ 		
+		numberPeriodePicker = (NumberPicker) findViewById(R.id.customNumberPeriode);
+		numberPeriodePicker.setMinValue(2);
+		numberPeriodePicker.setMaxValue(4);
+		numberPeriodePicker.setValue(periode.getNumberPeriode());
+		numberPeriodePicker.setOnValueChangedListener(this);
+ 		
+ 		cancelButton = (Button) findViewById(R.id.buttonCancel);
+ 		cancelButton.setOnClickListener(this);
+ 		saveButton = (Button) findViewById(R.id.buttonSave);
+ 		saveButton.setOnClickListener(this);
+ 		
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		if(v == saveButton) {
+			 getValue();
+		}else{
+			this.dismiss();
+		}
+	}
+
+	private void getPickerValue() {
+		
+		Periode periode = new Periode();
+		
+		switch (typePeriodePicker.getValue()) {
+		case 0:
+			periode.setTypePeriode(TypePeriode.WEEK);
+			break;
+		case 1:
+			periode.setTypePeriode(TypePeriode.MONTH);
+			break;
+		case 2:
+			periode.setTypePeriode(TypePeriode.TIMES);
+			break;
+		default:
+			break;
+		}
+		
+		periode.setNumberPeriode(getNumberPeriodePicker().getValue());
+		
+	}
+	
+	public abstract void getValue();
+	
+	
+
+	private void updateTitle() {
+		getPickerValue();
+		String title = typePeriodeArray[ getTypePeriodePicker().getValue() ] + " " + String.valueOf(getNumberPeriodePicker().getValue()) ;
+		setTitle(title);
+	}
+	@Override
+	public void onValueChange (NumberPicker np1, int oldVal, int newVal){
+		updateTitle();
+	}
+
+	public NumberPicker getTypePeriodePicker() {
+		return typePeriodePicker;
+	}
+
+	public void setTypePeriodePicker(NumberPicker typePeriodePicker) {
+		this.typePeriodePicker = typePeriodePicker;
+	}
+
+	public NumberPicker getNumberPeriodePicker() {
+		return numberPeriodePicker;
+	}
+
+	public void setNumberPeriodePicker(NumberPicker numberPeriodePicker) {
+		this.numberPeriodePicker = numberPeriodePicker;
+	}
+
+	public Periode getPeriode() {
+		return periode;
+	}
+
+	public void setPeriode(Periode periode) {
+		this.periode = periode;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
