a35e09bf01aa469e410427511bc4d5b4c46034af
gsourdrille@gmail.Com <gsourdrille@gmail.Com@12e82df5-f569-c33a-2569-eab8e1019727>
2013-10-24 21:30:02 +0000

    [DEV] mise en place du bouton checkbon

---
+++ TourDeGarde/src/fr/edenyorke/tourdegarde/utils/CollectionUtils.java
@@ -0,0 +1,11 @@
+package fr.edenyorke.tourdegarde.utils;
+
+import java.util.List;
+
+public class CollectionUtils {
+	
+	public final static boolean isEmpty(List<Object> values){
+		return values == null || values.isEmpty();
+	}
+
+}
