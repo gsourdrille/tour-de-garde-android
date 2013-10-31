9a9cf24265758ce212baba861141b5155e78bcc9
gsourdrille@gmail.com <gsourdrille@gmail.com@12e82df5-f569-c33a-2569-eab8e1019727>
2013-10-31 13:05:51 +0000

    [DEV] fin de la gestion des dates

--- trunk/TourDeGarde/src/fr/edenyorke/tourdegarde/MainActivity.java
+++ trunk/TourDeGarde/src/fr/edenyorke/tourdegarde/MainActivity.java
@@ -19,9 +19,10 @@ import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnDateChangedListener{
+public class MainActivity extends Activity implements OnDateChangedListener, OnValueChangeListener{
 	
 	
 	private DatePicker datePicker;
@@ -66,6 +67,7 @@ public class MainActivity extends Activity implements OnDateChangedListener{
     }
     
     private void initView(){
+    	
     	datePicker = (DatePicker) findViewById(R.id.datePicker);
     	datePicker.setSpinnersShown(true);
  		datePicker.setCalendarViewShown(false);
@@ -74,6 +76,7 @@ public class MainActivity extends Activity implements OnDateChangedListener{
  		inDayPicker.setMinValue(0);
  		inDayPicker.setMaxValue(1);
  		inDayPicker.setDisplayedValues( new String[] { "AM", "PM" } );
+ 		inDayPicker.setOnValueChangedListener(this);
  		
  		image = (ImageView) findViewById(R.id.resultImageView);
  		
@@ -90,6 +93,10 @@ public class MainActivity extends Activity implements OnDateChangedListener{
  		// set current date into datepicker
  		datePicker.init(year, month, day, this);
  		
+ 		//max Date
+ 		c.add(Calendar.YEAR, 5);
+ 		datePicker.setMaxDate(c.getTimeInMillis());
+ 		
  
   
  	}
@@ -98,11 +105,19 @@ public class MainActivity extends Activity implements OnDateChangedListener{
 	@Override
 	public void onDateChanged(DatePicker view, int year, int monthOfYear,
 			int dayOfMonth) {
+		calculerDate();
 		
+	}
+	@Override
+	public void onValueChange (NumberPicker np1, int oldVal, int newVal){
+		calculerDate();
+	}
+
+	private void calculerDate() {
 		Calendar dateSelected = Calendar.getInstance();
-		dateSelected.set(Calendar.YEAR, view.getYear());
-		dateSelected.set(Calendar.MONTH, view.getMonth());
-		dateSelected.set(Calendar.DAY_OF_MONTH, view.getDayOfMonth());
+		dateSelected.set(Calendar.YEAR, datePicker.getYear());
+		dateSelected.set(Calendar.MONTH, datePicker.getMonth());
+		dateSelected.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
 		
 		if(inDayPicker.getValue() == 0){
 			dateSelected.set(Calendar.HOUR_OF_DAY, 8);
@@ -116,14 +131,13 @@ public class MainActivity extends Activity implements OnDateChangedListener{
 		if(CollectionUtils.isEmpty(listeGarde)){
 			//affichage message d'erreur
 		}else{
-			boolean gardeOk = false;
+			Garde gardeInDate = null;
 			
 			//On commence par chercher les garde sans periode
 			List<Garde> listeGardeSansPeriode = GardeUtils.getGardeSansPeriode(listeGarde);
 			if(!CollectionUtils.isEmpty(listeGardeSansPeriode)){
-				Garde gardeInDate = GardeUtils.isDateBeetween(listeGardeSansPeriode, dateSelected.getTime());
+				gardeInDate = GardeUtils.isDateBeetween(listeGardeSansPeriode, dateSelected);
 				if(gardeInDate != null){
-					gardeOk = true;
 					image.setVisibility(View.VISIBLE);
 					nameGarde.setVisibility(View.VISIBLE);
 					nameGarde.setText(getResources().getString(R.string.name_garde, gardeInDate.getName()));
@@ -136,14 +150,39 @@ public class MainActivity extends Activity implements OnDateChangedListener{
 				}
 				
 			}
-			if(!gardeOk){
-				image.setVisibility(View.INVISIBLE);
-				nameGarde.setVisibility(View.INVISIBLE);
+			
+			if(gardeInDate == null){
+				List<Garde> listeGardeAvecPeriode = GardeUtils.getGardeAvecPeriode(listeGarde);
+				if(!CollectionUtils.isEmpty(listeGardeAvecPeriode)){
+					gardeInDate = GardeUtils.isDateBeetweenAvecPeriode(listeGardeAvecPeriode, dateSelected);
+					if(gardeInDate != null){
+						image.setVisibility(View.VISIBLE);
+						nameGarde.setVisibility(View.VISIBLE);
+						nameGarde.setText(getResources().getString(R.string.name_garde, gardeInDate.getName()));
+						if(gardeInDate.isEstPeriodeDeGarde()){
+							image.setImageResource(R.drawable.istourdegarde);
+						}else{
+							image.setImageResource(R.drawable.isnottourdegarde);
+						}
+						
+					}else{
+						gardeInDate = GardeUtils.isNotDateBeetweenAvecPeriode(listeGardeAvecPeriode, dateSelected);
+						if(gardeInDate != null){
+							image.setVisibility(View.VISIBLE);
+							nameGarde.setVisibility(View.VISIBLE);
+							nameGarde.setText(getResources().getString(R.string.name_garde, gardeInDate.getName()));
+							if(gardeInDate.isEstPeriodeDeGarde()){
+								image.setImageResource(R.drawable.isnottourdegarde);
+							}else{
+								image.setImageResource(R.drawable.istourdegarde);
+							}
+							
+						}
+					}
+				}
 			}
 			
-			
 		}
-		
 	}
     
 }
